46442595a26547f690b184ee6e88c1a5b64ff6ac
widowblack11 <o.prokopenko@notamedia.com.ru>
2023-09-16 20:27:12 +0300

    add param status_code

--- generic/helpers/account.py
+++ generic/helpers/account.py
@@ -9,8 +9,9 @@ class Account:
     def set_headers(self, headers):
         self.facade.account_api.client.session.headers.update(headers)
 
-    def register_new_user(self, login: str, email: str, password: str):
+    def register_new_user(self, login: str, email: str, password: str, status_code):
         response = self.facade.account_api.post_v1_account(
+            status_code=status_code,
             json=Registration(
                 login=login,
                 email=email,

8ffec8524f9d153284d4ad1d144f4d24b5151888
widowblack11 <o.prokopenko@notamedia.com.ru>
2023-09-21 23:30:59 +0300

    allure_attach for ORM and DB clients

--- db_client/db_client.py
+++ db_client/db_client.py
@@ -1,5 +1,7 @@
 import uuid
+from functools import wraps
 
+import allure
 import records
 import structlog
 
@@ -10,12 +12,32 @@ structlog.configure(
 )
 
 
+def allure_attach(func):
+    @wraps(func)
+    def wrapper(self, query):
+        allure.attach(
+            str(query.compile(compile_kwargs={"literal_binds": True})),
+            name='query',
+            attachment_type=allure.attachment_type.TEXT
+        )
+        result = func(self, query)
+        if result is not None:
+            allure.attach(
+                str(result),
+                name='query_result',
+                attachment_type=allure.attachment_type.TEXT
+            )
+        return result
+    return wrapper
+
+
 class DbClient:
     def __init__(self, user, password, host, database, isolation_level='AUTOCOMMIT'):
         connection_string = f"postgresql://{user}:{password}@{host}/{database}"
         self.db = records.Database(connection_string, isolation_level=isolation_level)
         self.log = structlog.get_logger(self.__class__.__name__).bind(sevice='db')
 
+    @allure_attach
     def send_query(self, query):
         print(query)
         log = self.log.bind(event_id=str(uuid.uuid4()))
@@ -30,6 +52,7 @@ class DbClient:
         )
         return dataset
 
+    @allure_attach
     def send_bulk_query(self, query):
         print(query)
         log = self.log.bind(event_id=str(uuid.uuid4()))
@@ -38,7 +61,3 @@ class DbClient:
             query=query
         )
         self.db.bulk_query(query=query)
-
-
-
-
--- orm_client/orm_client.py
+++ orm_client/orm_client.py
@@ -1,5 +1,7 @@
 import uuid
+from functools import wraps
 
+import allure
 import records
 import structlog
 from sqlalchemy import create_engine
@@ -9,6 +11,25 @@ structlog.configure(
         structlog.processors.JSONRenderer(indent=4, sort_keys=True, ensure_ascii=False)
     ]
 )
+
+
+def allure_attach(func):
+    @wraps(func)
+    def wrapper(self, query):
+        allure.attach(
+            str(query.compile(compile_kwargs={"literal_binds": True})),
+            name='query',
+            attachment_type=allure.attachment_type.TEXT
+        )
+        result = func(self, query)
+        if result is not None:
+            allure.attach(
+                str(result),
+                name='query_result',
+                attachment_type=allure.attachment_type.TEXT
+            )
+        return result
+    return wrapper
 
 
 class OrmClient:
@@ -22,6 +43,7 @@ class OrmClient:
     def close_connection(self):
         self.db.close()
 
+    @allure_attach
     def send_query(self, query):
         print(query)
         log = self.log.bind(event_id=str(uuid.uuid4()))
@@ -37,6 +59,7 @@ class OrmClient:
         )
         return result
 
+    @allure_attach
     def send_bulk_query(self, query):
         print(query)
         log = self.log.bind(event_id=str(uuid.uuid4()))
